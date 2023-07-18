3e73eb8eda915cdee921efa76cece23850d10d77
sumehar75 <139052938+sumehar75@users.noreply.github.com>
2023-07-18 23:54:55 +0530

    Add files via upload

---
+++ assignment operators.py
@@ -0,0 +1,25 @@
+#assignment operators
+# =,+=,-=,*=,**=,/=,//=,%=
+
+age = 20
+age = age + 3
+print("age is:",age)
+
+age += 3
+print("age is:",age)
+
+age += 10
+print("age is:",age)
+age -= 5
+print("age is:",age)
+age %= 3
+print("age is:",age)
+
+
+#increment decrement operators
+# does not exist in python
+
+idx = 0
+idx+=3
+idx-=1
+print("index is :", idx)
