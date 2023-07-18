87a45d5f383b099f5e1b337735fabc6c53e78a95
sumehar75 <139052938+sumehar75@users.noreply.github.com>
2023-07-18 23:53:03 +0530

    Add files via upload

---
+++ arithmatic operators.py
@@ -0,0 +1,27 @@
+#operators
+
+#arithmatic perators
+# +,-,*,**,/,//,%
+
+product_price = 125.25
+taxes = 0.18
+
+#assoiativity and precidence of operators
+price_to_pay = product_price + product_price * taxes
+print("price_to_pay: \u20b9", price_to_pay)
+
+numbers = 10
+numbers = numbers/3      #floating point div
+print(numbers)
+
+number = 10
+number = number//3      #integral div
+print(number)
+
+
+base = 2
+result = base * 2
+print("result:", result)
+
+result = base ** 4
+print("result:", result)
