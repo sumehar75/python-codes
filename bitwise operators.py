b939761d3f9efccc806b4cda654ee14372588f96
sumehar75 <139052938+sumehar75@users.noreply.github.com>
2023-07-18 23:56:30 +0530

    Add files via upload

---
+++ bitwise operators.py
@@ -0,0 +1,17 @@
+#bitwise operators
+# &, |, ^
+
+num1 = 10
+num2 = 8
+
+print("num1 binary:", bin(num1))
+print("num12binary:", bin(num2))
+
+result1 = num1 & num2
+print("result1 is:", result1)   #1000
+
+result2 = num1 | num2
+print("result2 is:", result2)     #1010
+
+result3 = num1 ^ num2
+print("result3 is:", result3)       #0010
